d608baeb311a1cd35210873dd726f1b8a1c4e64c
Jacob Woodward <jgw96@byu.edu>
2026-01-28 14:46:42 -0700

    Add main program logic to run Tic-Tac-Toe game and handle player turns

--- mission4/Program.cs
+++ mission4/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Driver
+{
+    static void Main(string[] args)
+    {
+        // Welcome message
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        // Create the game board (9 positions)
+        string[] board = new string[9];
+
+        // Initialize board with numbers 1–9
+        for (int i = 0; i < board.Length; i++)
+        {
+            board[i] = (i + 1).ToString();
+        }
+
+        string currentPlayer = "X";
+        bool gameWon = false;
+
+        // Game loop
+        while (!gameWon)
+        {
+            // Ask player for move
+            Console.WriteLine($"Player {currentPlayer}, choose a position (1-9):");
+            int choice = int.Parse(Console.ReadLine()) - 1;
+
+            // Update board
+            board[choice] = currentPlayer;
+
+            // Print board (supporting class)
+            BoardHelper.PrintBoard(board);
+
+            // Check for winner (supporting class)
+            gameWon = BoardHelper.CheckWinner(board);
+
+            // Switch players
+            currentPlayer = (currentPlayer == "X") ? "O" : "X";
+        }
+
+        Console.WriteLine("Game over!");
+    }
+}
--- mission4/obj/Release/net10.0/mission4.AssemblyInfo.cs
+++ mission4/obj/Release/net10.0/mission4.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("mission4")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eca0efc096e3532537be18a19fd6cb4c4f0f5938")]
 [assembly: System.Reflection.AssemblyProductAttribute("mission4")]
